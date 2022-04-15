d2a200e58af037c4d6875a9f84aea0f4c25e7f62
jrodr117 <jrodr113@msudenver.edu>
2022-04-15 08:19:24 -0600

    Implemented one to many relationship with Bait type used to catch fishes in your collection and implemented read functionality. Also jumped the gun and made a forms.py for the next impelementation create

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 from django.urls import reverse
+
+BAITS = (
+    ('LB', 'Live Bait'),
+    ('L', 'Lure'),
+    ('S', 'Spinner'),
+    ('P', 'Power Bait'),
+    ('SE', 'Salmon Eggs')
+)
 # Create your models here.
 class Fish(models.Model):
     kind = models.CharField(max_length=50)
@@ -12,3 +20,15 @@ class Fish(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'fish_id': self.id})
+
+class Bait(models.Model):
+    type = models.CharField(
+        'Types of Bait Used',
+        max_length=2,
+        choices=BAITS,
+        default=BAITS[0][0]
+    )
+    fish = models.ForeignKey(Fish, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_type_display()}"
